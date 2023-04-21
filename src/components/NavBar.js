3f2c49b60abb2cf70b209ce16e45153cb7f81fe1
Austin <austinodhiambo64@gmail.com>
2023-04-21 12:30:30 +0300

    Updated Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,9 +2,9 @@ import React from "react";
 import {v4 as uuid} from "uuid"
 
 function NavBar() {
-  const links = ["Home", "About", "Projects"];
+  const links = ["home", "about", "projects"];
 
-  return <nav>{links.map((link)=> <a key={uuid()}href={`#${link}`}>{link}</a> )}</nav>;
+  return <nav>{links.map((link)=> <a key={uuid()} href={`#${link}`}>{link}</a> )}</nav>;
 }
 
 export default NavBar;
